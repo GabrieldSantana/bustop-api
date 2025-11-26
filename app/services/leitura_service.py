bbe0941d545b6d50d6b3a24d0afc4141d4cdce7a
Daniel Tavares de Lima Marcelino <78553124+Daniel-Tavares-de-Lima@users.noreply.github.com>
2025-11-25 21:09:06 -0300

    Add calculo do comfort status  para save_leitura

--- app/services/leitura_service.py
+++ app/services/leitura_service.py
@@ -1,17 +1,34 @@
-# app/services/leitura_service.py
 from typing import List
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from ..repositories import leitura_repository as repo
 from ..schemas.leitura_schema import LeituraCreate
+from datetime import datetime
+
+
+def calcular_status_conforto(temp: float, umid: float) -> str:
+    if 20 <= temp <= 26 and 30 <= umid <= 60:
+        return "confortável"
+    return "desconfortável"
 
 
 async def save_leitura(db: AsyncIOMotorDatabase, leitura_in: LeituraCreate) -> str:
-    # Aqui mantemos validação por Pydantic + regras extra se precisar.
-    # Se houver regras de negócio adicionais, aplique-as aqui.
+    # meta
+    status = calcular_status_conforto(leitura_in.temperatura, leitura_in.umidade)
+    meta = {"status_conforto": status}
+
+    # salvar no Mongo
     inserted_id = await repo.insert_leitura(
-        db, leitura_in.temperatura, leitura_in.umidade, leitura_in.pessoas
+        db=db,
+        parada_id=leitura_in.parada_id,
+        device_id=leitura_in.device_id,
+        temperatura=leitura_in.temperatura,
+        umidade=leitura_in.umidade,
+        pessoas=leitura_in.pessoas,
+        timestamp=leitura_in.timestamp,
+        meta=meta,
     )
+
     return inserted_id
 
 
